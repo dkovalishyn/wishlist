71656dd7c4f7d15e372f11095eb84fde60eeb678
Denys Kovalishyn <denys_kovalishyn@epam.com>
2018-03-15 13:53:31 +0200

    add animation to messages add homepage draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { NgDragDropModule } from 'ng-drag-drop';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { WishlistComponent } from './components/wishlist/wishlist.component';
@@ -23,6 +24,7 @@ import { DndListComponent } from './components/dnd-list/dnd-list.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     NgDragDropModule.forRoot()
   ],
--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -1,10 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition} from '@angular/animations';
 import {MessageService} from '../../services/message.service';
 
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
-  styleUrls: ['./message.component.css']
+  styleUrls: ['./message.component.scss'],
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({ transform: 'translateX(0)' })),
+      transition('void => *', [
+        style({ transform: 'translateX(-100%)'  }),
+        animate(200),
+      ]),
+      transition('* => void', [
+        style({ transform: 'translateX(100%)'  }),
+        animate(200),
+      ])
+    ])
+  ],
 })
 export class MessageComponent implements OnInit {
 
